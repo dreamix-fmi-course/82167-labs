276993b802d88e23183b87a789e598394ac4b909
vladoiliev02 <79850594+vladoiliev02@users.noreply.github.com>
2022-03-10 19:16:41 +0200

    startig week3

---
+++ Week1/src/labs/collections/Exercise.java
@@ -0,0 +1,38 @@
+package labs.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Exercise {
+
+    // T0) Write a method to insert an element into the array list at the first position
+    public <T> void insertFront(List<T> list, T element) {
+        list.add(0, element);
+    }
+
+    // T1) Write a method to retrieve an element (at a specified index) from a given list.
+    public <T> T getAt(List<T> list, int pos) {
+        return list.get(pos);
+    }
+
+    // T2) Write a method to remove the third element from an array list.
+    public <T> void removeThird(List<T> list) {
+        list.remove(3);
+    }
+
+    // T3) Write a method to search an element in a list.
+
+    // T4) Write a method to sort a given array list. (list of String/Integer/Dog).
+    // Implement Dog class with attribute breed and weight, sort the array by weight property.
+    // Tip: implement the task with Comparator and Comparable
+
+    // T5) Write a method to replace the second element of a ArrayList with the specified element.
+
+    // T6)?? Write a Java program to count the number of key-value (size) mappings in a map.
+
+    // T7) Write the following structure against aircraft tail number associate
+    // list of leg information (fromAirport, toAirport, date).
+    // Fill test information
+    // Extract legs inside list/set that have from/to airport for a specific airport
+    // (Example: All legs for airport LBSF)
+}
